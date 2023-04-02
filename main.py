d51d01b0ceb721e02b612492e3fc062860746917
Germs & Spices <123396762+tblst8080@users.noreply.github.com>
2023-04-02 18:34:01 +0800

    add all assets

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('external', 'sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
